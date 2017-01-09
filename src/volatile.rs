2c11f3a07c1858ffdc5dc83e4b59bd30a7bca356
LambdaXymox <lambda.xymox@gmail.com>
2017-01-09 16:31:52 -0600

    Make VolatileCell interface public.

--- src/volatile.rs
+++ src/volatile.rs
@@ -10,21 +10,21 @@ pub struct VolatileCell<T> where T: Copy {
 impl<T> VolatileCell<T> where T: Copy {
 	/// Create a cell with an initial value.
 	#[inline]
-	fn new(value: T) -> VolatileCell<T> {
+	pub fn new(value: T) -> VolatileCell<T> {
 		VolatileCell {
 			value: value,
 		}
 	}
 
 	#[inline]
-	fn get(&self) -> T {
+	pub fn get(&self) -> T {
 		unsafe {
 			ptr::read_volatile(&self.value)
 		}
 	}
 
 	#[inline]
-	fn set(&self, value: T) {
+	pub fn set(&self, value: T) {
 		unsafe {
 			let ptr = &self.value as *const T as *mut T;
 			ptr::write_volatile(ptr, value);
@@ -32,7 +32,7 @@ impl<T> VolatileCell<T> where T: Copy {
 	}
 
 	#[inline]
-	fn update<F>(&mut self, f: F) 
+	pub fn update<F>(&mut self, f: F) 
 		where F: FnOnce(&mut T) 
 	{
 		let mut value = self.get();
